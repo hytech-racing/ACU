7f4f9c514e0cdf7c53df722755c956e505dd6137
songyueli <songyuedavidli@gmail.com>
2025-03-03 00:51:35 -0500

    added etl delegate function inst and calls to state machine

--- lib/state_machine/include/AMSStateMachine.h
+++ lib/state_machine/include/AMSStateMachine.h
@@ -4,7 +4,7 @@
 /* From shared-firmware-types */
 #include "SharedFirmwareTypes.h"
 
-#include "ACUController.h"
+#include <etl/delegate.h>
 
 enum class ACUState_e
 {
@@ -17,39 +17,69 @@ enum class ACUState_e
 class AMSStateMachine
 {
 public:
-    AMSStateMachine() {};
+    AMSStateMachine(
+        etl::delegate<bool()> received_CCU_message,
+        etl::delegate<bool()> has_bms_fault,
+        etl::delegate<bool()> has_imd_fault,
+        etl::delegate<bool()> received_valid_shdn_out,
+        etl::delegate<void()> enable_cell_balancing,
+        etl::delegate<void()> disable_cell_balancing,
+        etl::delegate<void()> disable_watchdog,
+        etl::delegate<void()> reinitialize_watchdog,
+        etl::delegate<void()> disable_n_latch_en,
+        etl::delegate<void()> reset_latch
+    ) :
+    _received_CCU_message(received_CCU_message),
+    _has_bms_fault(has_bms_fault),
+    _has_imd_fault(has_imd_fault),
+    _received_valid_shdn_out(received_valid_shdn_out),
+    _enable_cell_balancing(enable_cell_balancing),
+    _disable_cell_balancing(disable_cell_balancing),
+    _disable_watchdog(disable_watchdog),
+    _reinitialize_watchdog(reinitialize_watchdog),
+    _disable_n_latch_en(disable_n_latch_en),
+    _reset_latch(reset_latch)
+    {
+        _current_state = ACUState_e::STARTUP;
+    };
 
-    /**
-     * This tick() function handles all the update logic for traversing states, and calls the functions
-     * of other classes as necessary.
-     * @pre Other systems are updated properly
-     * @pre All relevant data exists in the data structs (VCRInterfaceData, VCRSystemData, etc.)
-     * @param current_millis The system time, in millis. Passed in by the scheduler.
-     * @param system_data A reference to the global system data struct.
-     */
-    void tick_state_machine(unsigned long current_millis, const ACUCoreData_s &system_data);
+    void tick_state_machine(unsigned long current_millis);
 
     /**
      * @return current ACU state
     */
     ACUState_e get_state();
+
 private:
-    void _set_state();
+
+    void _set_state(ACUState_e new_state, unsigned long curr_millis);
 
     /**
      * The function run upon the entry of the car into a new state.
      * @param new_state The state in which we are entering.
      */
-    void handle_entry_logic_(ACUState_e new_state, unsigned long curr_millis);
+    void _handle_entry_logic_(ACUState_e new_state, unsigned long curr_millis);
     
     /**
      * The function run upon the exit of a state.
      * @param prev_state the state in which we are leaving.
      */
-    void handle_exit_logic_(ACUState_e prev_state, unsigned long curr_millis);
-
-
+    void _handle_exit_logic_(ACUState_e prev_state, unsigned long curr_millis);
+        
     ACUState_e _current_state;
+
+    // Lamdas for state machine abstraction, functions defined in main
+    etl::delegate<bool()> _received_CCU_message; 
+    etl::delegate<bool()> _has_bms_fault;
+    etl::delegate<bool()> _has_imd_fault;
+    etl::delegate<bool()> _received_valid_shdn_out;
+    /// @brief setters
+    etl::delegate<void()> _disable_cell_balancing;
+    etl::delegate<void()> _enable_cell_balancing;
+    etl::delegate<void()> _disable_watchdog;
+    etl::delegate<void()> _reinitialize_watchdog;
+    etl::delegate<void()> _disable_n_latch_en;
+    etl::delegate<void()> _reset_latch;
 };
 
 #endif
