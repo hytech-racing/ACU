4e83878941d80412aef7280b25f53318e15cb969
songyueli <songyuedavidli@gmail.com>
2025-03-06 12:39:33 -0500

    working initial state test cases

--- test/test_systems/test_acu_controller.h
+++ test/test_systems/test_acu_controller.h
@@ -1,4 +1,8 @@
 #include "gtest/gtest.h"
+
+#include <array>
+#include <stddef.h>
+
 #include "SharedFirmwareTypes.h"
 #include "ACU_Globals.h"
 #include "ACUController.h"
@@ -9,14 +13,39 @@ bool charging_enabled;
 
 TEST (ACUControllerTesting, initial_state) {
     charging_enabled = false;
-    ASSERT_EQ(controller.evaluate_accumulator(0, charging_enabled, {0}), ACUControllerData_s<num_chips>{});
-    ASSERT_EQ(controller.evaluate_accumulator(0, charging_enabled, {0}).has_fault, false);
-    ASSERT_EQ(controller.evaluate_accumulator(0, charging_enabled, {0}).charging_enabled, charging_enabled);
-    ASSERT_EQ(controller.evaluate_accumulator(0, charging_enabled, {0}).cell_balance_statuses, std::array<uint16_t, num_chips>{0});
+    std::array<uint16_t, num_chips> cb = {0};
+    uint32_t start_time = 0;
+
+    auto status = controller.evaluate_accumulator(start_time, charging_enabled, {0});
+
+    ASSERT_EQ(status.has_fault, false);
+    ASSERT_EQ(status.cell_balance_statuses, cb); 
+    ASSERT_EQ(status.charging_enabled, false);
+
+    ASSERT_EQ(status.ov_start_time, 0);
+    ASSERT_EQ(status.uv_start_time, 0);
+    ASSERT_EQ(status.cell_ot_start_time, 0);
+    ASSERT_EQ(status.board_ot_start_time, 0);
+    ASSERT_EQ(status.pack_uv_start_time, 0);
 }
 
 TEST (ACUControllerTesting, charging_state) {
-    
+     charging_enabled = true;
+    std::array<uint16_t, num_chips> cb = {0};
+    uint32_t start_time = 0;
+
+    auto status = controller.evaluate_accumulator(start_time, charging_enabled, {0});
+
+    ASSERT_EQ(status.has_fault, false);
+    ASSERT_EQ(status.cell_balance_statuses, cb); 
+    ASSERT_EQ(status.charging_enabled, false);
+
+    ASSERT_EQ(status.ov_start_time, 0);
+    ASSERT_EQ(status.uv_start_time, 0);
+    ASSERT_EQ(status.cell_ot_start_time, 0);
+    ASSERT_EQ(status.board_ot_start_time, 0);
+    ASSERT_EQ(status.pack_uv_start_time, 0);
+    const uint32_t time_arb = 2500;
 }
 
 TEST (ACUControllerTesting, faulted_state) {
