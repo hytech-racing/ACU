8fc17f2893648b0d57f745b1f1da3e94e4098533
songyueli <76572259+songyueli@users.noreply.github.com>
2025-03-10 16:14:47 -0400

    Feature/watchdog interface (#9)
    * Created separate LTC SPI Interface that works with LTC6811 Interface to execute write and read functions. Need to add MessageInterface
    
    * LTC6811 read and write complete, need to add print functions
    
    * Configurable BMSDriverGroup Interface Ready to Test
    
    * Removed gen. broadcast/address cmd with mem error
    
    * Fixed LTCSPIInterface to use std::arrays
    
    * On LTCSPIInt., start "private" funcs with _
    
    * _"private function on LTCSPIInterface imp.
    
    * Revert "New BMSDriverGroup Chip"
    
    * impl. acucontroller with voltage fault check
    
    * issues with parsing every 3rd voltage. fixing pieces of driver by removing serial prints. cleaning up test main
    
    * undid for loop in wakeup protocol
    
    * updated load_aux gpio_in variable bit manip
    
    * removed additional transfer16 idky it's there
    
    * added missing template and ;
    
    * moved controller struct to controller header
    
    * created main_msg_test for testing/unit tests
    
    * replaced using volt with Shared Firmware types
    
    * removed 10000 scaling when printing voltages
    
    * pushing up test code
    
    * made voltage unit change conversion
    
    * fixed board_thermistor count in driver header
    
    * created watchdog interface pulse function
    
    * fixed acu_controller watchdog header
    
    * update acu state reinstated
    
    * fixed load cell voltages index var
    
    * fixed store voltage data header
    
    * added sequential cell balancing to spi test
    
    * remove extra source in files
    
    * made acu controller a class
    
    * removed manual send for troubleshooting function in driver
    
    * filled constructor with default parameters
    
    * implemented skeleton so ACU Controller is a class
    
    * possibly fixed out of bounds index error
    
    * re-added print auxillaries on main spi test
    
    * modified bms driver variable mis assignment, still want to test on board
    
    * removed dependency on the Configuration file, included fault count in namespace
    
    * changed funcion order in ACU Controller
    
    * added a cell balance getter from acu controller
    
    * fixed small things in read data on driver
    
    * changed temperature sensor units to be mV instead of V, now getting correct the temps
    
    * added teensy ok pin which is just supposed to stay high if watchdog ok
    
    * created class WatchdogInterface for all relevant hardware components
    
    * finished up rest of necessary functions for imd, shdn, and latch
    
    * added init of timer variables in watchdog constructor
    
    * merged and removed extra include
    
    * removed msg test file
    
    * fixed acu cnostants and globals to have correct imports
    
    * developing main.cpp to use watchdog and bms data
    
    * setting up acu interface tasks
    
    * initialize interfaces using etl singleton and run kick watchdog implemented
    git push
    
    * removed acu ethernet interface and imp sys time
    
    * implemented watchdog stuff into main and interface tasks
    
    ---------
    
    Co-authored-by: Ben Hall <rcmast3r1@gmail.com>

--- include/ACU_Constants.h
+++ include/ACU_Constants.h
@@ -2,9 +2,24 @@
 #define ACU_CONSTANTS
 
 #include <cstddef>
+#include <stddef.h>
+#include <stdio.h>
+#include <iostream>
+#include <array>
 
 constexpr size_t NUM_CELLS = 126;
 constexpr size_t NUM_CHIPS = 12;
 constexpr size_t NUM_CHIP_SELECTS = 1;
+
+// Initialize chip_select, chip_select_per_chip, and address
+constexpr std::array<int, NUM_CHIP_SELECTS> CS = {10};
+constexpr std::array<int, NUM_CHIPS> CS_PER_CHIP = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
+constexpr std::array<int, NUM_CHIPS> ADDR = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}; // only for addressable bms chips
+
+/* Task Times */
+constexpr unsigned long WATCHDOG_KICK_INTERVAL = 10;                 // 10 ms = 100 Hz
+
+/* Message Interface */
+const uint32_t CAN_baudrate = 500000;
 
 #endif
---
+++ src/ACU_SystemTasks.cpp
@@ -0,0 +1,11 @@
+#include "ACU_SystemTasks.h"
+
+bool initialize_all_systems() {
+    // Initialize the ACU Controller
+    ACUControllerInstance<NUM_CELLS>::create();
+    ACUControllerInstance<NUM_CELLS>::instance().init(sys_time::hal_millis());
+    return true;
+}
+
+
+
