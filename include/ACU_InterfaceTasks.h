66bc4720510fe7335022093bc5e2b414e0682e00
songyueli <songyuedavidli@gmail.com>
2025-03-09 21:34:30 -0400

    setting up acu interface tasks

--- include/ACU_InterfaceTasks.h
+++ include/ACU_InterfaceTasks.h
@@ -6,11 +6,25 @@
 
 /* Interface Library Includes */
 #include "BMSDriverGroup.h"
+#include <ht_task.hpp>
 
 using chip_type = LTC6811_Type_e;
 
+/**
+ * Init Functions - to be called in setup
+ */
+ 
 void initialize_interfaces();
+void initialize_systems();
 
+/**
+ * This task will fetch the watchdog state from WatchdogSystem and write it to the watchdog pin.
+ */
+bool init_kick_watchdog(const unsigned long& sysMicros, const HT_TASK::TaskInfo& taskInfo);
+bool run_kick_watchdog(const unsigned long& sysMicros, const HT_TASK::TaskInfo& taskInfo);
+
+
+/* Miscellaneous debugging functions */
 void set_bit(uint16_t & value, uint8_t index, bool bitValue);
 
 #endif 
