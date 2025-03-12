c3b6e27df3bd0f2ce2c11bd441938e01acc320a3
bharmai <86177994+bharmai@users.noreply.github.com>
2025-03-11 20:47:26 -0400

    ACU Ethernet Interface WIP (#14)
    * acu make functions
    
    * changes based on pr + receive function

--- lib/interfaces/src/ACUEthernetInterface.cpp
+++ lib/interfaces/src/ACUEthernetInterface.cpp
@@ -3,38 +3,44 @@
 
 #include <algorithm>
 
-hytech_msgs_VCRData_s ACUEthernetInterface::make_vcr_data_msg(const VCRData_s &shared_state)
+hytech_msgs_ACUCoreData_s ACUEthernetInterface::make_acu_core_data_msg(const ACUCoreData_s &shared_state)
 {
-
-}
-
-void ACUEthernetInterface::receive_pb_msg_acu_all_data(const hytech_msgs_ACUAllData_s &msg_in, VCRData_s &shared_state)
-{
+    hytech_msgs_ACUCoreData_s out;
     
+    out.pack_voltage = shared_state.pack_voltage;
+    out.min_cell_voltage = shared_state.min_cell_voltage;
+    out.avg_cell_voltage = shared_state.avg_cell_voltage;
+    out.max_cell_temp = shared_state.max_cell_temp;
+
+    return out;
 }
 
-void ACUEthernetInterface::receive_pb_msg_acu_core_data(const hytech_msgs_ACUCoreData_s &msg_in, VCRData_s &shared_state, unsigned long curr_millis)
+hytech_msgs_ACUAllData_s ACUEthernetInterface::make_acu_all_data_msg(const ACUAllData_s &shared_state)
 {
-    
+    hytech_msgs_ACUAllData_s out;
+
+    for (uint32_t i = 0; i < out.voltages_count; ++i)
+    {
+        out.voltages[i] = shared_state.voltages[i];
+    }
+
+    for (uint32_t i = 0; i < out.cell_temperatures_count; ++i)
+    {
+        out.cell_temperatures[i] = shared_state.cell_temperatures[i];
+    }
+
+    for (uint32_t i = 0; i < out.board_humidities_count; ++i)
+    {
+        out.board_humidities[i] = shared_state.board_humidities[i];
+    }
+
+    return out;
 }
 
-void ACUEthernetInterface::receive_pb_msg_db(const hytech_msgs_MCUCommandData &msg_in, VCRData_s &shared_state, unsigned long curr_millis)
+void ACUEthernetInterface::receive_pb_msg_vcr(const hytech_msgs_VCRData_s &msg_in, ACUCoreData_s &shared_state)
 {
-    //TODO: Finish this function. This function could parse the message and put it into shared_state, but depending
-    //      on where things are defined, it might be cleaner for this function to simply return the new data. I do
-    //      not know yet. Definitely worth asking Ben.    
-}
-
-void ACUEthernetInterface::receive_pb_msg_vcf(const hytech_msgs_VCFData_s &msg_in, VCRData_s &shared_state, unsigned long curr_millis)
-{
-    
-}
-	
-void ACUEthernetInterface::copy_inverter_data(const InverterData_s &original, hytech_msgs_InverterData_s &destination)
-{
-    
-}
-
-void ACUEthernetInterface::handle_send_ethernet_data(const hytech_msgs_VCRData_s &data) {
-    handle_ethernet_socket_send_pb<(size_t) 1024>(_drivebrain_ip, _acu_data_port, _udp_socket, data, hytech_msgs_VCRData_s_fields);
+    shared_state.avg_cell_voltage = msg_in.ams_data.average_cell_voltage;
+    shared_state.max_cell_temp = msg_in.ams_data.max_temp_celsius;
+    shared_state.min_cell_voltage = msg_in.ams_data.min_cell_voltage;
+    shared_state.pack_voltage = msg_in.ams_data.total_pack_voltage;
 }
