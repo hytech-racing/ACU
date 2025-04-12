8278a55fe1531032708f7eb4a8725b9da130e028
Justin Hwang <justin@goliath.org>
2025-04-12 06:45:19 -0400

    fixed ports in ACU ethernet interface (and IPs)

--- lib/interfaces/src/ACUEthernetInterface.cpp
+++ lib/interfaces/src/ACUEthernetInterface.cpp
@@ -5,7 +5,7 @@
 
 void ACUEthernetInterface::init_ethernet_device() {
     EthernetIPDefsInstance::create();
-    Ethernet.begin(EthernetIPDefsInstance::instance().acu_ip,  EthernetIPDefsInstance::instance().car_subnet, EthernetIPDefsInstance::instance().default_gateway);
+    Ethernet.begin(EthernetIPDefsInstance::instance().acu_ip, EthernetIPDefsInstance::instance().car_subnet, EthernetIPDefsInstance::instance().default_gateway);
     _acu_core_data_send_socket.begin(EthernetIPDefsInstance::instance().ACUCoreData_port);
     _acu_all_data_send_socket.begin(EthernetIPDefsInstance::instance().ACUAllData_port);
     _vcr_data_recv_socket.begin(EthernetIPDefsInstance::instance().VCRData_port);
@@ -14,19 +14,19 @@ void ACUEthernetInterface::init_ethernet_device() {
 
 void ACUEthernetInterface::handle_send_ethernet_acu_all_data(const hytech_msgs_ACUAllData &data) {
 
-    handle_ethernet_socket_send_pb<hytech_msgs_ACUAllData_size>(EthernetIPDefsInstance::instance().acu_ip, 
-                                                EthernetIPDefsInstance::instance().DBData_port,
+    handle_ethernet_socket_send_pb<hytech_msgs_ACUAllData_size>(EthernetIPDefsInstance::instance().drivebrain_ip, 
+                                                EthernetIPDefsInstance::instance().ACUAllData_port,
                                                 &_acu_all_data_send_socket, data, hytech_msgs_ACUAllData_fields);
 }
 
 void ACUEthernetInterface::handle_send_ethernet_acu_core_data(const hytech_msgs_ACUCoreData &data) {
-    handle_ethernet_socket_send_pb<hytech_msgs_ACUCoreData_size>(EthernetIPDefsInstance::instance().acu_ip, 
-                                                EthernetIPDefsInstance::instance().VCRData_port,
+    handle_ethernet_socket_send_pb<hytech_msgs_ACUCoreData_size>(EthernetIPDefsInstance::instance().drivebrain_ip, 
+                                                EthernetIPDefsInstance::instance().ACUCoreData_port,
                                                 &_acu_core_data_send_socket, data, hytech_msgs_ACUCoreData_fields);
 
-    handle_ethernet_socket_send_pb<hytech_msgs_ACUCoreData_size>(EthernetIPDefsInstance::instance().acu_ip, 
-                                                EthernetIPDefsInstance::instance().DBData_port,
-                                                &_acu_core_data_send_socket, data, hytech_msgs_ACUCoreData_fields);
+    // handle_ethernet_socket_send_pb<hytech_msgs_ACUCoreData_size>(EthernetIPDefsInstance::instance().acu_ip, 
+    //                                             EthernetIPDefsInstance::instance().DBData_port,
+    //                                             &_acu_core_data_send_socket, data, hytech_msgs_ACUCoreData_fields);
 }
 
 hytech_msgs_ACUCoreData ACUEthernetInterface::make_acu_core_data_msg(const ACUCoreData_s &shared_state)
