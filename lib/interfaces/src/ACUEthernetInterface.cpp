e5de3b5a9eb716449f917b6c52e79e68af81eda1
songyueli <songyuedavidli@gmail.com>
2025-05-08 13:39:32 -0400

    uncommented acu core data send at 100Hz

--- lib/interfaces/src/ACUEthernetInterface.cpp
+++ lib/interfaces/src/ACUEthernetInterface.cpp
@@ -13,23 +13,16 @@ void ACUEthernetInterface::init_ethernet_device() {
 }
 
 void ACUEthernetInterface::handle_send_ethernet_acu_all_data(const hytech_msgs_ACUAllData &data) {
-
     handle_ethernet_socket_send_pb<hytech_msgs_ACUAllData_size>(EthernetIPDefsInstance::instance().drivebrain_ip, 
                                                 EthernetIPDefsInstance::instance().ACUAllData_port,
                                                 &_acu_all_data_send_socket, data, hytech_msgs_ACUAllData_fields);
 }
 
 void ACUEthernetInterface::handle_send_ethernet_acu_core_data(const hytech_msgs_ACUCoreData &data) {
-    // handle_ethernet_socket_send_pb<hytech_msgs_ACUCoreData_size>(EthernetIPDefsInstance::instance().drivebrain_ip, 
-    //                                             EthernetIPDefsInstance::instance().ACUCoreData_port,
-    //                                             &_acu_core_data_send_socket, data, hytech_msgs_ACUCoreData_fields);
-
-    handle_ethernet_socket_send_pb<hytech_msgs_ACUAllData_size>(EthernetIPDefsInstance::instance().drivebrain_ip, 
-                                                EthernetIPDefsInstance::instance().ACUAllData_port,
-                                                &_acu_all_data_send_socket, data, hytech_msgs_ACUAllData_fields);
-    // handle_ethernet_socket_send_pb<hytech_msgs_ACUCoreData_size>(EthernetIPDefsInstance::instance().drivebrain_ip, 
-    //                                             EthernetIPDefsInstance::instance().ACUCoreData_port,
-    //                                             &_acu_core_data_send_socket, data, hytech_msgs_ACUCoreData_fields);
+    // no TCP Ethernet, just UDP
+    handle_ethernet_socket_send_pb<hytech_msgs_ACUCoreData_size>(EthernetIPDefsInstance::instance().drivebrain_ip, 
+                                                EthernetIPDefsInstance::instance().ACUCoreData_port,
+                                                &_acu_core_data_send_socket, data, hytech_msgs_ACUCoreData_fields);
 }
 
 hytech_msgs_ACUCoreData ACUEthernetInterface::make_acu_core_data_msg(const ACUCoreData_s &shared_state)
