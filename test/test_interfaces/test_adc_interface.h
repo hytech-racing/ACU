19f7592c07b3dd238a8b68912c6c949bcc8c806d
Anonimous-User <95382802+Anonimous-User@users.noreply.github.com>
2025-10-16 23:19:28 -0400

    good boy; you are not that guy pal (platformio), you are not that guy explained the need for the -I flag

--- test/test_interfaces/test_adc_interface.h
+++ test/test_interfaces/test_adc_interface.h
@@ -4,37 +4,27 @@
 
 using namespace fakeit;
 
-ADCPinout_s pinout = {ACUConstants::adc_default_params::pinout::IMD_OK_PIN,
-                            ACUConstants::adc_default_params::pinout::PRECHARGE_PIN,
-                            ACUConstants::adc_default_params::pinout::SHDN_OUT_PIN,
-                            ACUConstants::adc_default_params::pinout::TS_OUT_FILTERED_PIN,
-                            ACUConstants::adc_default_params::pinout::PACK_OUT_FILTERED_PIN,
-                            ACUConstants::adc_default_params::pinout::BSPD_CURRENT_PIN,
-                            ACUConstants::adc_default_params::pinout::SCALED_24V_PIN
-                    };
+ADCPinout_s pinout = {ACUInterfaces::IMD_OK_PIN,
+                    ACUInterfaces::PRECHARGE_PIN,
+                    ACUInterfaces::SHDN_OUT_PIN,
+                    ACUInterfaces::TS_OUT_FILTERED_PIN,
+                    ACUInterfaces::PACK_OUT_FILTERED_PIN,
+                    ACUInterfaces::BSPD_CURRENT_PIN,
+                    ACUInterfaces::SCALED_24V_PIN};
 
-ADCConversions_s conversions = {ACUConstants::adc_default_params::conversions::SHUTDOWN_CONV_FACTOR,
-                                ACUConstants::adc_default_params::conversions::PRECHARGE_CONV_FACTOR,
-                                ACUConstants::adc_default_params::conversions::PACK_AND_TS_OUT_CONV_FACTOR,
-                                ACUConstants::adc_default_params::conversions::SHDN_OUT_CONV_FACTOR,
-                                ACUConstants::adc_default_params::conversions::BSPD_CURRENT_CONV_FACTOR,
-                                ACUConstants::adc_default_params::conversions::GLV_CONV_FACTOR
-                                };
+ADCConversions_s conversions = {ACUInterfaces::SHUTDOWN_CONV_FACTOR,
+                                ACUInterfaces::PRECHARGE_CONV_FACTOR,
+                                ACUInterfaces::PACK_AND_TS_OUT_CONV_FACTOR,
+                                ACUInterfaces::SHDN_OUT_CONV_FACTOR,
+                                ACUInterfaces::BSPD_CURRENT_CONV_FACTOR,
+                                ACUInterfaces::GLV_CONV_FACTOR};
 
-ADCThresholds_s thresholds = {ACUConstants::adc_default_params::thresholds::TEENSY41_MIN_DIGITAL_READ_VOLTAGE_THRESH,
-                                ACUConstants::adc_default_params::thresholds::TEENSY41_MAX_DIGITAL_READ_VOLTAGE_THRESH,
-                                ACUConstants::adc_default_params::thresholds::SHUTDOWN_VOLTAGE_DIGITAL_THRESHOLD
-                            };
-
-ADCConfigs_s configs = {ACUConstants::adc_default_params::configs::IMD_STARTUP_TIME,
-                            ACUConstants::adc_default_params::configs::BIT_RESOLUTION,
-                            ACUConstants::adc_default_params::configs::TEENSY41_MAX_INPUT_VOLTAGE
-                        };
+float bit_res = ACUInterfaces::BIT_RESOLUTION;
 
 TEST (ADCInterfaceTesting, init) {
     When(Method(ArduinoFake(), pinMode)).AlwaysReturn();
 
-    ADCInterfaceInstance::create(pinout, conversions, thresholds, configs);
+    ADCInterfaceInstance::create(pinout, conversions, bit_res);
     ADCInterfaceInstance::instance().init(0);
 
     ASSERT_EQ(ADCInterfaceInstance::instance().is_in_imd_startup_period(), true);
@@ -52,7 +42,7 @@ TEST (ADCInterfaceTesting, read_imd_ok) {
     When(Method(ArduinoFake(), pinMode)).AlwaysReturn();
     When(Method(ArduinoFake(), analogRead)).AlwaysReturn(0);
 
-    ADCInterfaceInstance::create(pinout, conversions, thresholds, configs);
+    ADCInterfaceInstance::create(pinout, conversions, bit_res);
     ADCInterfaceInstance::instance().init(0);
 
     ADCInterfaceInstance::instance().read_imd_ok(1000);
@@ -71,7 +61,7 @@ TEST (ADCInterfaceTesting, read_functions) {
     When(Method(ArduinoFake(), pinMode)).AlwaysReturn();
     When(Method(ArduinoFake(), analogRead)).AlwaysReturn(0);
 
-    ADCInterfaceInstance::create(pinout, conversions, thresholds, configs);
+    ADCInterfaceInstance::create(pinout, conversions, bit_res);
     ADCInterfaceInstance::instance().init(3000);
     ADCInterfaceInstance::instance().read_shdn_voltage();
     ADCInterfaceInstance::instance().read_shdn_out_voltage();
