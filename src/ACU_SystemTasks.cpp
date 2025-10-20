f1a236414a5d222cea3f4f14610280f3a088989b
anonimous-user <harrisbond1234@gmail.com>
2025-10-20 00:36:09 -0400

    that shouldn't have worked?

--- src/ACU_SystemTasks.cpp
+++ src/ACU_SystemTasks.cpp
@@ -59,7 +59,7 @@ HT_TASK::TaskResponse evaluate_accumulator(const unsigned long &sysMicros, const
 {
     ACUControllerInstance<ACUConstants::NUM_CELLS, ACUConstants::NUM_CELL_TEMPS, ACUConstants::NUM_BOARD_TEMPS>::instance().evaluate_accumulator(sys_time::hal_millis(), BMSDriverInstance<ACUConstants::NUM_CHIPS, ACUConstants::NUM_CHIP_SELECTS, chip_type::LTC6811_1>::instance().get_acu_data()); // verified
     EMData_s em_data = EMInterfaceInstance::instance().get_latest_data(sys_time::hal_millis());
-    // ACUControllerInstance<ACUConstants::NUM_CELLS, ACUConstants::NUM_CELL_TEMPS, ACUConstants::NUM_BOARD_TEMPS>::instance().get_state_of_charge(em_data.em_current, em_data.time_since_prev_msg_ms);
+    ACUControllerInstance<ACUConstants::NUM_CELLS, ACUConstants::NUM_CELL_TEMPS, ACUConstants::NUM_BOARD_TEMPS>::instance().get_state_of_charge(em_data.em_current, em_data.time_since_prev_msg_ms);
     return HT_TASK::TaskResponse::YIELD;
 }
 
