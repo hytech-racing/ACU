d2117cc9b8b0dd9048a79c8e1192269028d31a93
anonimous-user <harrisbond1234@gmail.com>
2025-10-27 00:04:41 -0400

    On car, updated periods

--- include/ACU_Constants.h
+++ include/ACU_Constants.h
@@ -69,9 +69,9 @@ namespace ACUConstants
     /* Task Times */
     constexpr uint32_t TICK_SM_PERIOD_US = 1000UL; // 1 000 us = 1000 Hz
     constexpr uint32_t TICK_SM_PRIORITY = 9;
-    constexpr uint32_t KICK_WATCHDOG_PERIOD_US = 100UL; // 100 us = 1 000 Hz | to compensate for delays, otherwise get 20ms watchdog periods->bad
+    constexpr uint32_t KICK_WATCHDOG_PERIOD_US = 5000UL; // 5000 us = 200 Hz
     constexpr uint32_t WATCHDOG_PRIORITY = 1;
-    constexpr uint32_t SAMPLE_BMS_PERIOD_US = 3000UL; // Original: 20 000 us = 50 Hz; New: 3 000 us = 300 Hz (since we are reading by group)
+    constexpr uint32_t SAMPLE_BMS_PERIOD_US = 4000UL; // 3 000 us = 300 Hz (since we are reading by group)
     constexpr uint32_t SAMPLE_BMS_PRIORITY = 2;
     constexpr uint32_t EVAL_ACC_PERIOD_US = 20000UL; // 20 000 us = 50 Hz
     constexpr uint32_t EVAL_ACC_PRIORITY = 10;
