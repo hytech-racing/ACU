12ab26badda8e675a44c95d6bb2700fd6334627e
songyueli <songyuedavidli@gmail.com>
2025-11-07 18:55:11 -0500

    removed ds2480b interface cuz don't work

--- src/main_em_temp_test.cpp
+++ src/main_em_temp_test.cpp
@@ -1,175 +1,25 @@
-// #include <DS18B20.h>
-#include <DS2480B.h>
+#include <DS18B20.h>
 
-// DS18B20 ds(2);
+DS18B20 ds(2);
 
-// void setup() {
-//   Serial.begin(115200);
-//   Serial.print("Devices: ");
-//   Serial.println(ds.getNumberOfDevices());
-//   Serial.println();
-// }
-
-// void loop() {
-//   while (ds.selectNext()) {
-//     Serial.println(ds.getTempC());
-//   }
-//   Serial.print("Resolution: ");
-//   Serial.println(ds.getResolution());
-
-//   Serial.print("Power Mode: ");
-//   if (ds.getPowerMode()) {
-//     Serial.println("External");
-//   } else {
-//     Serial.println("Parasite");
-//   }
-// }
-
-// AltSoftSerial altSerial; //RX 8 - TX 9
-// Serial main_serial;
-
-DS2480B ds(Serial2);
-
-void setup(void) {
+void setup() {
   Serial.begin(115200);
-  Serial2.begin(9600);
-  // altSerial.begin(9600);
-  Serial.println("SETUP");
-  ds.begin();
+  Serial.print("Devices: ");
+  Serial.println(ds.getNumberOfDevices());
+  Serial.println();
 }
 
-void loop(void) {
-  byte i;
-  byte present = 0;
-  byte type_s;
-  byte data[12];
-  byte addr[8];
-  float celsius, fahrenheit;
-  
-  if ( !ds.search(addr)) {
-    Serial.println("No more addresses.");
-    Serial.println();
-    ds.reset_search();
-    delay(5000);
-    return;
+void loop() {
+  while (ds.selectNext()) {
+    Serial.println(ds.getTempC());
   }
-  
-  Serial.print("ROM =");
-  for( i = 0; i < 8; i++) {
-    Serial.write(' ');
-    Serial.print(addr[i], HEX);
-  }
+  Serial.print("Resolution: ");
+  Serial.println(ds.getResolution());
 
-  if (DS2480B::crc8(addr, 7) != addr[7]) {
-      Serial.println("CRC is not valid!");
-      return;
-  }
-  Serial.println();
- 
-  // the first ROM byte indicates which chip
-  switch (addr[0]) {
-    case 0x10:
-      Serial.println("  Chip = DS18S20");  // or old DS1820
-      type_s = 1;
-      break;
-    case 0x28:
-      Serial.println("  Chip = DS18B20");
-      type_s = 0;
-      break;
-    case 0x22:
-      Serial.println("  Chip = DS1822");
-      type_s = 0;
-      break;
-    default:
-      Serial.println("Device is not a DS18x20 family device.");
-      return;
-  } 
-  
-  ds.reset();
-  
-  delay(100);
-
-  ds.reset();
-  ds.select(addr);
-  ds.write(0x44);        // start conversion
-  
-  delay(1000);     // maybe 750ms is enough, maybe not
-  // we might do a ds.depower() here, but the reset will take care of it.
-  
-  present = ds.reset();
-  ds.select(addr);    
-  ds.write(0xBE);         // Read Scratchpad
-
-  Serial.print("  Data = ");
-  Serial.print(present, HEX);
-  Serial.print(" ");
-  for ( i = 0; i < 9; i++) {           // we need 9 bytes
-    data[i] = ds.read();
-    Serial.print(data[i], HEX);
-    Serial.print(" ");
-  }
-  Serial.print(" CRC=");
-  Serial.print(DS2480B::crc8(data, 8), HEX);
-  Serial.println();
-
-  // Convert the data to actual temperature
-  // because the result is a 16 bit signed integer, it should
-  // be stored to an "int16_t" type, which is always 16 bits
-  // even when compiled on a 32 bit processor.
-  int16_t raw = (data[1] << 8) | data[0];
-  if (type_s) {
-    raw = raw << 3; // 9 bit resolution default
-    if (data[7] == 0x10) {
-      // "count remain" gives full 12 bit resolution
-      raw = (raw & 0xFFF0) + 12 - data[6];
-    }
+  Serial.print("Power Mode: ");
+  if (ds.getPowerMode()) {
+    Serial.println("External");
   } else {
-    byte cfg = (data[4] & 0x60);
-    // at lower res, the low bits are undefined, so let's zero them
-    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
-    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
-    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
-    //// default is 12 bit resolution, 750 ms conversion time
+    Serial.println("Parasite");
   }
-  celsius = (float)raw / 16.0;
-  fahrenheit = celsius * 1.8 + 32.0;
-  Serial.print("  Temperature = ");
-  Serial.print(celsius);
-  Serial.print(" Celsius, ");
-  Serial.print(fahrenheit);
-  Serial.println(" Fahrenheit");
 }
-
-// #include <DS18B20.h>
-
-// #define LOW_ALARM 20
-// #define HIGH_ALARM 25
-
-// DS18B20 ds(2);
-// uint8_t address[] = {40, 250, 31, 218, 4, 0, 0, 52};
-// uint8_t selected;
-
-// void setup() {
-//   Serial.begin(9600);
-//   selected = ds.select(address);
-
-//   if (selected) {
-//     ds.setAlarms(LOW_ALARM, HIGH_ALARM);
-//   } else {
-//     Serial.println("Device not found!");
-//   }
-// }
-
-// void loop() {
-//   if (selected) {
-//     if (ds.hasAlarm()) {
-//       Serial.print("Warning! Temperature is ");
-//       Serial.print(ds.getTempC());
-//       Serial.println(" C");
-//     }
-//   } else {
-//     Serial.println("Device not found!");
-//   }
-
-//   delay(10000);
-// }
