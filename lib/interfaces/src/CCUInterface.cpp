88b9698200329869881e50de940e0aa3cbe6c4d5
Advait Vedant <advait.suraj.vedant@gmail.com>
2025-11-09 17:37:05 -0800

    added NOLINT in CCUInterface lines to prevent check error from using array pointers

--- lib/interfaces/src/CCUInterface.cpp
+++ lib/interfaces/src/CCUInterface.cpp
@@ -37,20 +37,17 @@ void CCUInterface::handle_enqueue_acu_cell_voltages_CAN_message(
     const size_t num_of_chips
 ) {
     BMS_CELL_VOLTAGES_t msg = {};
-    size_t num_of_groups, num_of_voltage_cells;
-    bool voltage_cell_group_cycle_loop_completed;
-
     msg.chip_id = static_cast<uint8_t>(_curr_data.current_voltage_group_chip_id);
     msg.cell_group_id = static_cast<uint8_t>(_curr_data.current_voltage_cell_group_id);
 
-    num_of_groups = voltage_cells_per_chip[_curr_data.current_voltage_group_chip_id] / _ccu_params.voltage_cells_per_group;
-    num_of_voltage_cells = std::accumulate(voltage_cells_per_chip, voltage_cells_per_chip + num_of_chips, 0);
+    size_t num_of_groups = voltage_cells_per_chip[_curr_data.current_voltage_group_chip_id] / _ccu_params.voltage_cells_per_group; //NOLINT
+    size_t num_of_voltage_cells = std::accumulate(voltage_cells_per_chip, voltage_cells_per_chip + num_of_chips, 0); //NOLINT
 
-    msg.cell_group_voltage_0_ro = HYTECH_cell_group_voltage_0_ro_toS(cell_voltages[_curr_data.current_voltage_cell_id]); 
-    msg.cell_group_voltage_1_ro = HYTECH_cell_group_voltage_1_ro_toS(cell_voltages[_curr_data.current_voltage_cell_id + 1]);
-    msg.cell_group_voltage_2_ro = HYTECH_cell_group_voltage_2_ro_toS(cell_voltages[_curr_data.current_voltage_cell_id + 2]);
+    msg.cell_group_voltage_0_ro = HYTECH_cell_group_voltage_0_ro_toS(cell_voltages[_curr_data.current_voltage_cell_id]); //NOLINT
+    msg.cell_group_voltage_1_ro = HYTECH_cell_group_voltage_1_ro_toS(cell_voltages[_curr_data.current_voltage_cell_id + 1]); //NOLINT
+    msg.cell_group_voltage_2_ro = HYTECH_cell_group_voltage_2_ro_toS(cell_voltages[_curr_data.current_voltage_cell_id + 2]); //NOLINT
 
-    voltage_cell_group_cycle_loop_completed = _increment_and_loop_id(_curr_data.current_voltage_cell_group_id, num_of_groups);
+    size_t voltage_cell_group_cycle_loop_completed = _increment_and_loop_id(_curr_data.current_voltage_cell_group_id, num_of_groups);
 
     if (voltage_cell_group_cycle_loop_completed) _increment_and_loop_id(_curr_data.current_voltage_group_chip_id, num_of_chips);
 
@@ -64,20 +61,17 @@ void CCUInterface::handle_enqueue_acu_cell_temps_CAN_message(
     const size_t num_of_chips
 ) {
     BMS_CHIP_TEMPS_t chip_temps_msg = {};
-    size_t num_of_groups, num_of_temp_cells;
-    bool voltage_cell_group_cycle_loop_completed;
 
     chip_temps_msg.chip_id = static_cast<uint8_t>(_curr_data.current_temp_group_chip_id);
     chip_temps_msg.thermistor_group_id = static_cast<uint8_t>(_curr_data.current_temp_group_id);
 
-    num_of_groups = temp_cells_per_chip[_curr_data.current_temp_group_chip_id] / _ccu_params.temp_cells_per_group;
-    num_of_temp_cells = std::accumulate(temp_cells_per_chip, temp_cells_per_chip + num_of_chips, 0);
+    size_t num_of_groups = temp_cells_per_chip[_curr_data.current_temp_group_chip_id] / _ccu_params.temp_cells_per_group; //NOLINT
+    size_t num_of_temp_cells = std::accumulate(temp_cells_per_chip, temp_cells_per_chip + num_of_chips, 0); //NOLINT
 
-    chip_temps_msg.thermistor_cell_group_temp_0_ro = HYTECH_thermistor_cell_group_temp_0_ro_toS(cell_temps[_curr_data.current_temp_cell_id]); 
-    chip_temps_msg.thermistor_cell_group_temp_1_ro = HYTECH_thermistor_cell_group_temp_1_ro_toS(cell_temps[_curr_data.current_temp_cell_id + 1]);
+    chip_temps_msg.thermistor_cell_group_temp_0_ro = HYTECH_thermistor_cell_group_temp_0_ro_toS(cell_temps[_curr_data.current_temp_cell_id]); //NOLINT
+    chip_temps_msg.thermistor_cell_group_temp_1_ro = HYTECH_thermistor_cell_group_temp_1_ro_toS(cell_temps[_curr_data.current_temp_cell_id + 1]); //NOLINT
 
-    bool temp_cell_group_cycle_loop_completed;
-    temp_cell_group_cycle_loop_completed = _increment_and_loop_id(_curr_data.current_temp_group_id, num_of_groups);
+    bool temp_cell_group_cycle_loop_completed = _increment_and_loop_id(_curr_data.current_temp_group_id, num_of_groups);
 
     if (temp_cell_group_cycle_loop_completed) _increment_and_loop_id(_curr_data.current_temp_group_chip_id, num_of_chips);
 
@@ -106,7 +100,7 @@ void CCUInterface::handle_enqueue_acu_board_temps_CAN_message(
 ) {
     BMS_ONBOARD_CURRENT_TEMP_t current_board_temp_msg = {};
     current_board_temp_msg.chip_id = _curr_data.current_temp_board_id;
-    current_board_temp_msg.temp_0_ro = HYTECH_temp_0_ro_toS(board_temps[_curr_data.current_temp_board_id]);
+    current_board_temp_msg.temp_0_ro = HYTECH_temp_0_ro_toS(board_temps[_curr_data.current_temp_board_id]); //NOLINT
     _increment_and_loop_id(_curr_data.current_temp_board_id, num_of_boards);
     CAN_util::enqueue_msg(&current_board_temp_msg, &Pack_BMS_ONBOARD_CURRENT_TEMP_hytech, ACUCANBuffers::ccu_can_tx_buffer);
 }
