90d3c7dab2e023ce9a61731295b34df066e6633b
songyueli <songyuedavidli@gmail.com>
2026-03-29 20:36:34 -0400

    comment print

--- src/main.cpp
+++ src/main.cpp
@@ -71,11 +71,10 @@ void setup()
     
     scheduler.schedule(sample_adc_task);
 
-    scheduler.schedule(debug_prints_task);
+    // scheduler.schedule(debug_prints_task);
 
     handle_CAN_setup(ACUCANInterfaceImpl::CCU_CAN, ACUConstants::Veh_CAN_baudrate, &ACUCANInterfaceImpl::on_ccu_can_receive);
     handle_CAN_setup(ACUCANInterfaceImpl::EM_CAN, ACUConstants::EM_CAN_baudrate, &ACUCANInterfaceImpl::on_em_can_receive);
-
 }
 
 void loop()
