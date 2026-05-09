57b046503b538f2bc98ab7a919d2639b016688e1
mansi-b29 <133283027+mansi-b29@users.noreply.github.com>
2026-05-09 15:57:58 -0400

    wip of reading through temp data for each sensor

--- lib/systems/include/TempSensorDriver.h
+++ lib/systems/include/TempSensorDriver.h
@@ -14,14 +14,27 @@ struct TempSensorData_s {
     float temp_6 = 0;
 };
 
-struct SensorIDs_s {
-    uint8_t addr_1;
-    uint8_t addr_2;
-    uint8_t addr_3;
-    uint8_t addr_4;
-    uint8_t addr_5;
-    uint8_t addr_6;
+// hard coded ROM IDs of DS1820Bs based on addresses collected with an arduino when validating the harneses in the accumulator 
+uint8_t SensorIDs [6][8] = {
+    {0x28, 0xA6, 0xF5, 0x10, 0x11, 0x00, 0x00, 0x5D}, 
+    {0x28, 0xF9, 0x5A, 0x71, 0x11, 0x00, 0x00, 0x14}, 
+    {0x28, 0x75, 0x42, 0x11, 0x11, 0x00, 0x00, 0x51},
+    {0x28, 0x6B, 0xCE, 0x70, 0x11, 0x00, 0x00, 0xEC},
+    {0x28, 0xE0, 0xF4, 0x70, 0x11, 0x00, 0x00, 0xC7},
+    {0x28, 0xC8, 0x92, 0x70, 0x11, 0x00, 0x00, 0x5D}
 };
+
+    /*  address 1: 40 166 245 16 17 0 0 93
+        address 2: 40 249 90 113 17 0 0 20 
+        address 3: 40 117 66 17 17 0 0 81
+        address 4: 40 107 206 112 17 0 0 236
+        address 5: 40 224 244 112 17 0 0 199
+        address 6: 40 200 146 112 17 0 0 93
+
+        first byte 
+    */
+
+
 class TempSensorDriver {
 public: 
 
@@ -34,6 +47,9 @@ public:
     DS2480B oneWire; //set port??
     uint8_t raw_temp_data[9];
     int16_t converted_data = 0;
+
+private:
+    uint8_t temp_sensor_data;
 };
 
 
--- lib/systems/src/TempSensorDriver.cpp
+++ lib/systems/src/TempSensorDriver.cpp
@@ -8,23 +8,41 @@ void TempSensorDriver::init() {
 
 TempSensorData_s TempSensorDriver::get_temps() {
 
-    oneWire.beginTransaction();
+    oneWire.beginTransaction(); // enter Data Mode, where you can send reset, pulse, configuraiton, and search commands
+
+    // every transaction must follow the sequence reset - ROM command - function, and repeat the cycle
 
     oneWire.reset(); // every transaction requires a reset
     oneWire.skip(); // skip ROM command - address all devices on the bus
-    oneWire.write(0x44); // temperature conversion byte (page 23 os DS2480B datasheet)
+    oneWire.write(0x44); // temperature conversion byte (page 23 os DS2480B datasheet) - tell all devices on the bus to convert temperatures
 
-    //include a delay in between 
+    //need to include a delay in between (?)
 
-    oneWire.reset(); 
     // need to address each sensor individually in order to read each of their data
-    oneWire.write(0xBE); // read scratchpad (where all of the data is stored)
+
+    for (int i = 0; i < 7; i++) { 
+        oneWire.reset();
+        oneWire.select(SensorIDs[i]);
+        oneWire.write(0xBE); // read scratchpad (where all of the data is stored) - must do this for each sensor
+    }
+
+    /*  address 1: 40 166 245 16 17 0 0 93
+        address 2: 40 249 90 113 17 0 0 20 
+        address 3: 40 117 66 17 17 0 0 81
+        address 4: 40 107 206 112 17 0 0 236
+        address 5: 40 224 244 112 17 0 0 199
+        address 6: 40 200 146 112 17 0 0 93
+    */
+
+
 
     for (int i = 0; i < 9; i++) {
         raw_temp_data[i] = oneWire.read();
     }
 
     converted_data = (raw_temp_data[1] << 8) | raw_temp_data[0];
+
+    oneWire.endTransaction(); // back to Command Mode
 
 
 }
